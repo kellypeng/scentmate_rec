8189ab14f74817a1a2679f191edc8fbeb0015580
kellypeng <kellypeng17@gmail.com>
2017-08-27 23:31:52 -0700

    remove MongoDB duplicates

---
+++ rm_mongo_coll_duplicates.js
@@ -0,0 +1,30 @@
+var duplicates = [];
+
+db.perfume_features.aggregate([
+  { $match: {
+    name: { "$ne": '' }  // discard selection criteria
+  }},
+  { $group: {
+    _id: { name: "$perfume_id"}, // can be grouped on multiple properties
+    dups: { "$addToSet": "$_id" },
+    count: { "$sum": 1 }
+  }},
+  { $match: {
+    count: { "$gt": 1 }    // Duplicates considered as count greater than one
+  }}
+],
+{allowDiskUse: true}       // For faster processing if set is larger
+)               // You can display result until this and check duplicates
+.forEach(function(doc) {
+    doc.dups.shift();      // First element skipped for deleting
+    doc.dups.forEach( function(dupId){
+        duplicates.push(dupId);   // Getting all duplicate ids
+        }
+    )
+})
+
+// If you want to Check all "_id" which you are deleting else print statement not needed
+printjson(duplicates);
+
+// Remove all duplicates in one go
+db.collectionName.remove({_id:{$in:duplicates}})
