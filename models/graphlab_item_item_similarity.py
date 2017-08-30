911338fbbc2a79c86d77bebd62b165fcf8c1f07b
kellypeng <kellypeng17@gmail.com>
2017-08-30 09:05:54 -0700

    update models

---
+++ models/graphlab_item_item_similarity.py
@@ -0,0 +1,44 @@
+import sys
+import pandas as pd
+import numpy as np
+import graphlab as gl
+import matplotlib.pyplot as plt
+from time import time
+import model_main as mm
+
+def mf_model(data, similarity_type='cosine', only_top_k=64):
+    '''Fit matrix factorization model'''
+    return gl.recommender.item_similarity_recommender.create(data,
+                                                            user_id='user_id',
+                                                            item_id='perfume_id',
+                                                            target='user_rating',
+                                                            nearest_items=None, # A set of each item’s nearest items. When provided, this overrides the similarity computed by only_top_k
+                                                            similarity_type=similarity_type,
+                                                            threshold=0.001,
+                                                            only_top_k=only_top_k)
+
+def tune_only_top_k(train, test):
+    '''Find the optimum number of Number of similar items to store for each item
+    and calculate RMSE against train test data'''
+    test_rmse = []
+    train_rmse = []
+    for n in xrange(30, 200, 5):
+        m = mf_model(train, only_top_k=n)
+        test_rmse.append(m.evaluate_rmse(test, target='user_rating')['rmse_overall'])
+        train_rmse.append(m.training_rmse)
+    plt.plot(test_rmse, color='r', label='Test RMSE')
+    plt.plot(train_rmse, color='b', label='Train RMSE')
+    plt.xlabel('Number of Factors')
+    plt.xlabel('RMSE')
+
+
+if __name__ == '__main__':
+    train = pd.read_csv('/Users/kellypeng/Documents/Tech/github/Galvanize/scent_cn_rec/data/train_valid_test/utility_train_train.csv')
+    valid = pd.read_csv('/Users/kellypeng/Documents/Tech/github/Galvanize/scent_cn_rec/data/train_valid_test/utility_train_valid.csv')
+    # convert to SFrame
+    train = gl.SFrame(train)
+    valid = gl.SFrame(valid)
+    tune_only_top_k(train, valid)
+    plt.show()
+
+    #### Try random_split_by_user in graphlab
---
+++ models/train_test_split.py
@@ -0,0 +1,47 @@
+import re
+import pandas as pd
+import numpy as np
+from pymongo import MongoClient
+
+def train_test_split(df, n=3, frac=0.25):
+    '''Filter out users with >3 ratings.
+    For users with 3 rating, put 2 ratings in training set, 1 rating in test set.
+    For users with more than 3 ratings, divide the total number of ratings by 4,
+    leave 1/4 in test set, all others in training set.
+
+    Parameters:
+    -----------
+    User rating DataFrame, already only kept users with >2 ratings
+
+    Returns:
+    --------
+    Training set, test set. DataFrame.
+    '''
+    training_df = pd.DataFrame()
+    test_df = pd.DataFrame()
+    df.set_index('perfume_id', inplace=True) # to avoid additional index column
+    for user_id in df['user_id'].unique():
+        user_ratings = df[df['user_id'] == user_id]
+        if user_ratings.groupby('user_id')['user_rating'].count().values == n:
+            sample = user_ratings.sample(n=1, axis=0)
+            test_df = test_df.append(sample)
+            training_df = training_df.append(user_ratings.drop(sample.index, axis=0))
+        else:
+            sample = user_ratings.sample(frac=frac, axis=0)
+            test_df = test_df.append(sample)
+            training_df = training_df.append(user_ratings.drop(sample.index, axis=0))
+    return training_df, test_df
+
+
+if __name__ == '__main__':
+    #utility = pd.read_csv('utility_matrix.csv')
+
+    #training_df, test_df = train_test_split(utility)
+    #training_df.to_csv('utility_train.csv', encoding='utf-8')
+    #test_df.to_csv('utility_test.csv', encoding='utf-8')
+
+    training_df = pd.read_csv('utility_train.csv')
+    print "splitting training df..."
+    train_train_df, train_valid_df = train_test_split(training_df, n=2, frac=0.34)
+    train_train_df.to_csv('utility_train_train.csv', encoding='utf-8')
+    train_valid_df.to_csv('utility_train_valid.csv', encoding='utf-8')
